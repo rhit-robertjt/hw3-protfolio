53ff5712bd66dd45b6830e661d4699b782b811be
rhit-robertjt <98368458+rhit-robertjt@users.noreply.github.com>
2025-04-03 22:28:30 -0400

    Interactive (dark mode)

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,6 +1,21 @@
-// document.addEventListener("DOMContentLoaded", setupListener);
-// document.addEventListener("DOMContentLoaded", addExistingList);
+function main() {
+  document.querySelector('#light-mode').addEventListener("click", () => {
+    const root = document.documentElement;
+    root.style.setProperty('--main-bg-color', '#AD1414');
+    root.style.setProperty('--second-bg-color', '#333');
+    root.style.setProperty('--main-color', 'white');
+  });
+  document.querySelector('#dark-mode').addEventListener("click", () => {
+    const root = document.documentElement;
+    root.style.setProperty('--main-bg-color', '#333');
+    root.style.setProperty('--second-bg-color', '#AD1414');
+    root.style.setProperty('--main-color', 'black');
+  });
+}
+
+// document.addEventListener("DOMContentLoaded", main);
 window.addEventListener("load", (event) => {
     console.log("Event Occurred: " + event);
     console.log("Page fully loaded.");
+    main();
 })
